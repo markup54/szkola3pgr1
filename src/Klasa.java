31dee5a7303ab075b8fd7ee5d46f63f4c0971162
markup54 <marzenakupka@tlen.pl>
2025-10-27 15:38:34 +0100

    klasa

--- src/Klasa.java
+++ src/Klasa.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Klasa {
+import java.util.ArrayList;
+
+public class Klasa {
+    private Wychowawca wychowawca;
+    private ArrayList<Uczen> uczniowie;
+    private String nazwaKlasy;
+    //myślimy co ta klasa powinna "wiedziec"
+
+
+    public Klasa(Wychowawca wychowawca, ArrayList<Uczen> uczniowie, String nazwaKlasy) {
+        this.wychowawca = wychowawca;
+        this.uczniowie = uczniowie;
+        this.nazwaKlasy = nazwaKlasy;
+    }
+
+    public Klasa(String nazwaKlasy) {
+        this.nazwaKlasy = nazwaKlasy;
+        uczniowie = new ArrayList<>();
+    }
 }
